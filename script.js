2fb276c578845f5c8a16e2488e01ec6baf4a1608
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-21 12:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	let ans = [];
+	for (let value of arr) {
+		ans.push(value+1);
+	}
+	return ans
 }
 
 alert(incrementArray(arr));
